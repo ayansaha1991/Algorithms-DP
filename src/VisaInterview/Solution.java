e3e0ca4753c7ad554f0dd9b47170e2665ffb511c
ayansaha1991 <ayansaha1991@gmail.com>
2017-09-16 13:29:39 +0530

    HackerRank: https://www.hackerrank.com/challenges/reduced-string/problem

--- src/VisaInterview/Solution.java
+++ src/VisaInterview/Solution.java
@@ -3,21 +3,25 @@ import java.util.Scanner;
 
 public class Solution {
 
-    static String super_reduced_string(String s){
+	static String super_reduced_string(String s) {
+
+		boolean isComplete = false;
 		
-    	for (int i=0 ; i < s.length()-1; i++) {
-    		
-    		if (s.charAt(i) == s.charAt(i+1)) {
-    			String temp = s.substring(i, i+2);
-    			s = s.replaceFirst(temp, "");
-    			
-    			if (  super_reduced_string(s).equals(s) ) {
-        			return s;
-        		} 
-    		}
-    	}
-    	return s;
-    }
+		for (int i = 0; i < s.length() - 1; i++) {
+			
+			if (s.charAt(i) == s.charAt(i + 1)) {
+				String temp = s.substring(i, i + 2);
+				s = s.replaceFirst(temp, "");
+				isComplete = true;
+			}
+		}
+		
+		if (!isComplete) {
+			return s.isEmpty() ? "Empty String" : s;
+		}
+
+		return super_reduced_string(s);
+	}
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
